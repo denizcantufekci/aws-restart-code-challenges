a350264e08b1eacd66f79eb1aea208f0b262c055
Deniz Can Tüfekçi <denizcantufekci@gmail.com>
2023-08-01 15:00:42 +0300

    challenges done

---
+++ Challenge-03.py
@@ -0,0 +1,51 @@
+"""
+Task: 
+
+Write a Python program to get a string from a given string where all occurrences of its first char have been changed to '$', except the first char itself.
+Sample String : 'restart
+Expected Result : 'resta$t
+"""
+
+# Solution:
+
+def replace_first_repeated(input_string):
+    
+    #if an empty string is passed, return an empty string
+    if not input_string:
+        return ""
+
+    char_count = {}
+    first_replaced_char = ""
+    char_replaced = True
+    result_string = input_string
+    index = 0
+
+    for char in input_string:
+        index += 1
+
+        if char in char_count:
+            char_count[char] += 1
+            
+            if char_count[char] > 1:
+                if char_replaced:
+                    first_replaced_char = char
+                    char_replaced = False
+
+            if first_replaced_char == char:
+                result_string = result_string[:index-1] + '$' + result_string[index:]
+
+        else:
+            char_count[char] = 1
+
+    return result_string
+
+# Test the function
+sample_string = 'rressstttaarrrtt'
+result = replace_first_repeated(sample_string)
+print("Sample String:", sample_string)
+print("Expected Result:", "r$essstttaa$$$tt")
+print("Result:", result)
+
+# User Input Case
+given_string = input("Enter a string: ")
+print(replace_first_repeated(given_string))
---
+++ Challenge-04.py
@@ -0,0 +1,50 @@
+"""
+Task: 
+
+Write a Python program to get a string made of the first 2 and the last 2 chars from a given a string. 
+If the string length is less than 2, return instead of the empty string.
+"""
+
+# Solution:
+
+def get_specific_chars(input_string):
+        
+        if not input_string:
+            return ""
+        if len(input_string) < 2:
+            return ""
+        
+        first_two_chars = input_string[:2]
+        last_two_chars = input_string[-2:]
+
+        return first_two_chars + last_two_chars
+
+# Test the function
+sample_string = "Empty String"
+result = get_specific_chars(sample_string)
+print("Sample String 1: ", sample_string)
+print("Expected Result 1:", result)
+
+sample_string = ""
+result = get_specific_chars(sample_string)
+print("Sample String 2: ", sample_string)
+print("Expected Result 2:", result)
+
+sample_string = "A"
+result = get_specific_chars(sample_string)
+print("Sample String 3: ", sample_string)
+print("Expected Result 3:", result)
+
+sample_string = "AW"
+result = get_specific_chars(sample_string)
+print("Sample String 4: ", sample_string)
+print("Expected Result 4:", result)
+
+sample_string = "AWS"
+result = get_specific_chars(sample_string)
+print("Sample String 5: ", sample_string)
+print("Expected Result 5:", result)
+
+# User Input Case
+given_string = input("Enter a string: ")
+print(get_specific_chars(given_string))
